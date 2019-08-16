031d6fe9b4ae68cd29f72fe0becb089c8a2f3199
kuangshp <kuangshp@126.com>
2019-08-16 14:23:35 +0800

    新增表格渲染

--- src/views/demo2/Demo2.vue
+++ src/views/demo2/Demo2.vue
@@ -1,15 +1,58 @@
 <template>
   <div class="demo2">
-    <h1>我是demo2页面</h1>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+      >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Provide, Vue } from 'vue-property-decorator';
 @Component({
   components: {},
 })
-export default class Demo2 extends Vue { }
+export default class Demo2 extends Vue {
+  @Provide() private data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+      }],
+    };
+  }
+}
 </script>
 <style lang="less">
 </style>
