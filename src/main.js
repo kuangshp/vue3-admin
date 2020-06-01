2f1eb36fa5d0fb83d3084364bbc96b1af96e389a
kuangshp <kuangshp@126.com>
2020-06-01 08:41:26 +0800

    新增环境变量的配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// 引入全局样式
+import './assets/css/main.scss';
+
 /******************************** 引入elementUi组件库 start ********************************/
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
