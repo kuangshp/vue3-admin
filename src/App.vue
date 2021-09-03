11ba865d3152f57b4d1dbaa239e8a1bc557b0754
kuangshp <kuangshp@126.com>
2021-09-03 14:49:50 +0800

    feat: 基础的配置

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <img src="./assets/logo.png" />
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
+  <el-button type="primary">按钮</el-button>
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -22,13 +15,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,10 +1,14 @@
 import { App } from 'vue';
 import ElementPlus from 'element-plus';
+// 单独引入一个
+// import { ElButton } from 'element-plus';
 import '../element-variables.scss';
 import locale from 'element-plus/lib/locale/lang/zh-cn';
 import 'dayjs/locale/zh-cn';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (app: App) => {
+export default (app: App): void => {
+  // 全局注册全部的组件
   app.use(ElementPlus, { locale });
+  // 全局配置大小
+  app.config.globalProperties.$ELEMENT = { size: 'small' };
 };
