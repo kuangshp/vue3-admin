7bd1be8fe442b6044762be603c810b34b7cb3bd8
kuangshp <kuangshp@126.com>
2022-01-21 13:52:56 +0800

    feat: 初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,6 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
