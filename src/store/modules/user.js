7a70dd698f66ffb986e09b9ad28f6056449c9482
kuangshp <kuangshp@126.com>
2022-01-21 22:00:57 +0800

    feat: user状态管理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,17 +1,18 @@
-// import { TOKEN } from '@/constant';
 import * as types from '../mutation-types';
+import { storage } from '@/utils';
+import { authToken } from '@/constants';
 // import router from '@/router';
 export default {
   namespaced: true, // 命名模块
   state: () => ({
-    token: '',
+    token: storage.getItem(authToken) || '',
     userInfo: {},
   }),
   mutations: {
     [types.SET_TOKEN](state, token) {
       state.token = token;
       // 存储到本地
-      // setItem(TOKEN, token);
+      storage.setItem(authToken, token);
     },
     [types.SET_USER_INFO](state, info) {
       state.userInfo = info;
