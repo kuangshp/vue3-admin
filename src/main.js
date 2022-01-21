4b2c58b43e20709a9684649aac662521f3e6f323
kuangshp <kuangshp@126.com>
2022-01-21 16:46:53 +0800

    feat(icons): 新增svg字体图标

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import installElementPlus from './plugins/element';
+import installIcons from './icons';
 
 const app = createApp(App);
 installElementPlus(app);
+installIcons(app);
 
 app.use(store).use(router).mount('#app');
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,8 @@
 <template>
   登录页面
   <el-button type="primary">el-button</el-button>
+  <svg-icon id="guide-start" icon="guide" />
+  <svg-icon icon="user"></svg-icon>
 </template>
 
 <script setup></script>
