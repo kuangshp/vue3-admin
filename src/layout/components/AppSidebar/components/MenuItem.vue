9efe0a94f9ab188861774972b78b5bc8a869c558
kuangshp <kuangshp@126.com>
2022-01-24 10:50:55 +0800

    feat: 处理兼容性

--- src/layout/components/AppSidebar/components/MenuItem.vue
+++ src/layout/components/AppSidebar/components/MenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i>
+  <i v-if="icon && icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i>
   <svg-icon v-else :icon="icon"></svg-icon>
   <span>{{ title }}</span>
 </template>
