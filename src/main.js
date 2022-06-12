579d1009c3303b3760810bbf3573105b960d4ccd
kuangshp <kuangshp@126.com>
2022-06-12 17:16:10 +0800

    feat: 登录页面的开发

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router';
 import store from './store';
 import installElementPlus from './plugins/element';
 
+// 导入全局样式
+import './assets/styles/index.scss';
+
 const app = createApp(App);
 installElementPlus(app);
 app.use(store).use(router).mount('#app');
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,130 @@
 <template>
-  <div>登录</div>
+  <div class="login-container">
+    <el-form class="login-form" :model="loginForm" ref="loginForm">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <el-icon><Avatar /></el-icon>
+        </span>
+        <el-input placeholder="请输入用户名" name="username" type="text" v-model="loginForm.username" />
+      </el-form-item>
+
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <el-icon><Avatar /></el-icon>
+        </span>
+        <el-input placeholder="请输入密码" name="password" v-model="loginForm.password" :type="passwordType"></el-input>
+        <span class="show-pwd">
+          <el-icon><Avatar /></el-icon>
+        </span>
+      </el-form-item>
+
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px" :loading="loading" @click="handleLogin">登录</el-button>
+    </el-form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const loginForm = ref({
+  username: 'admin',
+  password: '123456',
+});
 
-<style lang="scss" scoped></style>
+const loading = ref(false);
+
+// 登录操作
+const handleLogin = () => {};
+</script>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+    .tips {
+      font-size: 16px;
+      color: white;
+      line-height: 24px;
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+  ::v-deep .lang-select {
+    position: absolute;
+    top: 4px;
+    right: 0;
+    background-color: #fff;
+    font-size: 22px;
+    padding: 4px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+}
+</style>
