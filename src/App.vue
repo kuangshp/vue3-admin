61d68ca236cbab90f41f468172bb9cd060823135
kuangshp <kuangshp@126.com>
2022-06-12 17:02:23 +0800

    feat: 引入element-plus依赖

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <router-view />
+  <img src="./assets/logo.png" />
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style></style>
