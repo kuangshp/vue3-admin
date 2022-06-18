526b885d87df6fb3739d5bac68659b2ac8628780
kuangshp <kuangshp@126.com>
2022-06-18 08:23:57 +0800

    feat: 用户主动退出方案

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,7 +1,70 @@
 <template>
-  <div>导航</div>
+  <div class="navbar">
+    <div class="right-menu">
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar shape="square" :size="40"></el-avatar>
+          <i class="el-icon-s-tools"></i>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item>首页</el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click="logout">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from 'vuex';
+const store = useStore();
+const logout = () => {
+  console.log('点击了退出');
+  store.dispatch('user/logout');
+};
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  .right-menu {
+    display: flex;
+    align-items: center;
+    float: right;
+    padding-right: 16px;
+    ::v-deep .avatar-container {
+      cursor: pointer;
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+        .el-avatar {
+          margin-right: 12px;
+        }
+      }
+    }
+    ::v-deep .right-menu-item {
+      display: inline-block;
+      padding: 0 18px 0 0;
+      font-size: 24px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+
+      &.hover-effect {
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
