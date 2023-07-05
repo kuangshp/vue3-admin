c3a4a15fec9cc67875789a59f2b0a7338280bb02
shuiping.kuang <kuangshp@126.com>
2023-07-05 14:01:12 +0800

    feat: 中文语音包

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <el-config-provider :locale="zhCn">
     <RouterView />
-  </div>
+  </el-config-provider>
 </template>
 
-<script setup></script>
+<script setup>
+  import { ElConfigProvider } from 'element-plus';
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+</script>
 
 <style scoped lang="scss"></style>
